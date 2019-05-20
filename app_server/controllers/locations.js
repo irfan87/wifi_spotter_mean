df927b40b35a7bec23d547ec4ae1d10da8cdadfe
irfan87 <nerve2009@yahoo.com>
2019-05-20 16:00:14 +0800

    done desiged the main page

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -1,5 +1,30 @@
 const homeList = (req, res, next) => {
-    res.render('locations-list', {title: 'Home'});
+    res.render('locations-list', {
+        title: 'Wifi Locator - find a place to work with wifi',
+        pageHeader: {
+            title: 'Wifi Locator',
+            strapLine: 'Find a place to work with wifi'
+        },
+        locations: [{
+            name: 'Starcups',
+            address: '125 High Street, Reading, RG6 1PS',
+            rating: 3,
+            facilities: ['Hot Drinks', 'Food', 'Premium Wifi'],
+            distance: '100m'
+        }, {
+            name: 'Cafe Hero',
+            address: '125 High Street, Reading, RG6 1PS',
+            rating: 4,
+            facilities: ['Hot Drinks', 'Food', 'Premium Wifi'],
+            distance: '50m'
+        }, {
+            name: 'Burger Queen',
+            address: '125 High Street, Reading, RG6 1PS',
+            rating: 2,
+            facilities: ['Food', 'Premium Wifi'],
+            distance: '250m'
+        }]
+    });
 }
 
 const locationInfo = (req, res, next) => {
