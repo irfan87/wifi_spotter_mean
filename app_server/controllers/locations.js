d97a6e7cd80d14562a80e65deb78b9c2f111f038
irfan87 <nerve2009@yahoo.com>
2019-05-23 00:52:02 +0800

    done refactoring the view and controller

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -1,3 +1,8 @@
+const env = require('dotenv').config();
+
+const GOOGLE_API_KEY = process.env.GOOGLE_API_KEY
+
+// outlet listing that will shown at the main page
 const homeList = (req, res, next) => {
     res.render('locations-list', {
         title: 'Wifi Locator - find a place to work with wifi',
@@ -28,12 +33,66 @@ const homeList = (req, res, next) => {
     });
 }
 
+// it will show the specific location with the id
+// such as /location/starbucks
 const locationInfo = (req, res, next) => {
-    res.render('location-info', {title: 'Location Info'});
+    res.render('location-info', {
+        title: 'Starcups',
+        pageHeader: {
+            title: 'Starcups'
+        },
+        google_api_key: GOOGLE_API_KEY,
+        sidebar: {
+            context: 'is on Wifi Locator because it has accessible wifi and space to sit down with your laptop and get some work done.',
+            callToAction: 'If you\'ve been and you like it - or if you don\'t - please leave a review to help other people just like you.',
+        },
+        location: {
+            name: 'Starcups',
+            address: '125 High Street, Reading, RG6 1PS',
+            rating: 3,
+            facilities: ['Hot Drinks', 'Food', 'Premium Wifi'],
+            coords: {lat: 51.455041, lng: -0.9690884},
+            openingTimes: [{
+                days: 'Monday - Friday',
+                opening: '7:00am',
+                closing: '7:00pm',
+                closed: false
+            }, {
+                days: 'Saturday',
+                opening: '8:00am',
+                closing: '5:00pm',
+                closed: false
+            }, {
+                days: 'Sunday',
+                closed: true
+            }],
+            reviews: [{
+                author: 'Tony Stark',
+                rating: 4,
+                timestamp: '16 July 2017',
+                reviewText: 'This place is so damn awesome and relaxing. It really remind me being with my love one'
+            }, {
+                author: 'Charlie Chaplin', 
+                rating: 3,
+                timestamp: '15 June 2018',
+                reviewText: 'This is so relaxing but hope if you guys could sell anything than a few types of cake.'
+            }, {
+                author: 'John Legend',
+                rating: 1,
+                timestamp: '21 May 2019',
+                reviewText: 'This place should concern the speed of the internet'
+            }]
+        }
+    });
 }
 
 const addReview = (req, res, next) => {
-    res.render('location-review-form', {title: 'Add Review'});
+    res.render('location-review-form', {
+        title: 'Review Starcups on Wifi Locator',
+        pageHeader: {
+            title: 'Review Starcups'
+        }
+    });
 }
 
 module.exports = {
--- app_server/controllers/others.js
+++ app_server/controllers/others.js
@@ -1,5 +1,8 @@
 const about = (req, res, next) => {
-    res.render('generic-text', {title: 'About'});
+    res.render('generic-text', {
+        title: 'About Us',
+        content: 'Wifi Locator was created to help people to find places to sit down and get a bit work done.<br /><br />Lorem ipsum dolor sit amet, consectetur adipisicing elit. Libero ratione error quasi excepturi quae, similique, necessitatibus eaque alias vitae fuga laborum totam tempora consequuntur nesciunt saepe ad ullam vero placeat?<br /><br />Lorem ipsum dolor sit, amet consectetur adipisicing elit. Facilis reiciendis sapiente excepturi reprehenderit vero nulla deserunt esse ut culpa eligendi nobis aspernatur veritatis explicabo, saepe ratione, alias dicta eum aliquam?'
+    });
 }
 
 module.exports = {
