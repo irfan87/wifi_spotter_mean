3cffe1e8aadc5d51775abfd1e471b6fd9ae80593
irfan87 <nerve2009@yahoo.com>
2019-05-28 23:19:24 +0800

    add code to monito database behavior, either in localhost or in cloud

--- app_server/models/db.js
+++ app_server/models/db.js
@@ -1,19 +1,32 @@
 const mongoose = require('mongoose');
-const readline = require('readline');
 const dbURI = "mongodb://localhost:27017/wifi_locator";
 mongoose.connect(dbURI, {useNewUrlParser: true});
 
-// check if the OS like Windows can support with SIGINT
-if(process.platform === 'win32') {
-    const rl = readline.createInterface({
-        input: process.stdin,
-        output: process.stdout
-    });
-
-    rl.on('SIGINT', () => {
-        process.emit("SIGINT");
-    });
+// Mongoose graceful shutdown
+const gracefulShutDown = (msg, callback) => {
+    mongoose.connection.close(() => {
+        console.log(`Mongoose disconnected through ${msg}`);
+        callback();
+    })
 }
+
+process.once('SIGUSR2', () => {
+    gracefulShutDown('nodemon restart', () => {
+        process.kill(process.pid, 'SIGUSR2');
+    });
+});
+
+process.on('SIGINT', () => {
+    gracefulShutDown('app termination', () => {
+        process.exit(0);
+    });
+});
+
+process.on('SIGTERM', () => {
+    gracefulShutDown('Heroku app shutdown', () => {
+        process.exit(0);
+    });
+});
 
 mongoose.connection.on('connected', () => {
     console.log(`Mongoose is connected to ${dbURI}`);
